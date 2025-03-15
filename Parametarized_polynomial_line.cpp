eb1be908a16ab6799c272ff5f3ac9e2bee361ac8
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2025-03-15 16:04:30 +0200

    edited code for assignment submission

--- Parametarized_polynomial_line.cpp
+++ Parametarized_polynomial_line.cpp
@@ -1,8 +1,5 @@
 #include<Windows.h>
-#include<math.h>
-// #include <windef.h>
 #include <math.h>
-#include <windef.h>
 
 using namespace std;
 
@@ -18,7 +15,7 @@ int max(double x, double y) {
     }
 }
 
-void ParametarizedPolynomialLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c1, COLORREF c2) {
+void InterpolatedColoredLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c1, COLORREF c2) {
     int alfa1 = x2 - x1;
     int alfa2 = y2 - y1;
     int beta1 = x1;
@@ -32,14 +29,6 @@ void ParametarizedPolynomialLine(HDC hdc, int x1, int y1, int x2, int y2, COLORR
     int betaB = GetBValue(c1);
 
     double step = 1.0/max(abs(alfa1), abs(alfa2));
-    // double stepR = 1.0/abs(alfaR);
-    // double stepG = 1.0/abs(alfaG);
-    // double stepB = 1.0/abs(alfaB);
-
-
-    // if(alfa1 == 0 || alfa2 == 0) {
-    //     return;
-    // }
     
     for (double t = 0; t <= 1; t += step) {
         double x = alfa1 * t + beta1;
@@ -48,10 +37,6 @@ void ParametarizedPolynomialLine(HDC hdc, int x1, int y1, int x2, int y2, COLORR
         double r = alfaR * t + betaR;
         double g = alfaG * t + betaG;
         double b = alfaB * t + betaB;
-
-        // alfaR += stepR;
-        // alfaG += stepG;
-        // alfaB += stepB;
         
         SetPixel(hdc, round(x), round(y), RGB(round(r), round(g), round(b)));
     }
@@ -104,8 +89,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT m, WPARAM wp, LPARAM lp) {
             x2 = LOWORD(lp);
             y2 = HIWORD(lp);
 
-            ParametarizedPolynomialLine(hdc, x1, y1, oldx2, oldy2, RGB(192, 192, 192), RGB(192, 192, 192));
-            ParametarizedPolynomialLine(hdc, x1, y1, x2, y2, c1, c2);
+            InterpolatedColoredLine(hdc, x1, y1, oldx2, oldy2, RGB(192, 192, 192), RGB(192, 192, 192));
+            InterpolatedColoredLine(hdc, x1, y1, x2, y2, c1, c2);
         }
 		
 
@@ -133,9 +118,9 @@ int APIENTRY WinMain(HINSTANCE hi, HINSTANCE pi, LPSTR cmd, int nsh) {
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Fixed incorrect cursor
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW); 
     wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-    wc.lpszClassName = TEXT("MyClass"); // Use TEXT() for compatibility
+    wc.lpszClassName = TEXT("MyClass");
     wc.lpszMenuName = NULL;
     wc.lpfnWndProc = WndProc;
     wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -145,7 +130,7 @@ int APIENTRY WinMain(HINSTANCE hi, HINSTANCE pi, LPSTR cmd, int nsh) {
 
     HWND hwnd = CreateWindow(TEXT("MyClass"), TEXT("Hello World!"), WS_OVERLAPPEDWINDOW, 
                              CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
-                             NULL, NULL, hi, NULL); // Use TEXT()
+                             NULL, NULL, hi, NULL);
 
     ShowWindow(hwnd, nsh);
     UpdateWindow(hwnd);
