42965f41b8b85a6e4cfc71eb63c1633fb47b3c18
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2025-03-26 20:56:34 +0200

    finished diamond algo

--- Diamond_bresenham_line.cpp
+++ Diamond_bresenham_line.cpp
@@ -9,6 +9,7 @@ int roundLocal(double x) {
 }
 
 void BresenhamsEfficientDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c) {
+
 	int dx = abs(x2 - x1);
 	int dy = abs(y2 - y1);
 	int x = x1;
@@ -16,7 +17,6 @@ void BresenhamsEfficientDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
 
     double m = (double)dy / dx;
 
-	//int d1 = (dx * (2 * y + 1 - 2 * y1)) - (2 * dy * (x + 1 - x1));
     int d1 = dx - 2*dy;
 	int d1c1 = -2 * dy;
 	int d1c2 = 2 * (dx - dy);
@@ -135,57 +135,6 @@ void BresenhamsEfficientDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c)
             }
         }
     }
-}
-
-void parabolaDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c) {
-	int oldx = x1;
-	int oldy = y1;
-	int diff = 0;
-
-	int x = x1;
-	int y = x1 * x1;
-	int dy = 2 * x1 + 1;
-	int d2y = 2;
-
-	SetPixel(hdc, x1, y1, c);
-
-	while (x < x2) {
-		oldx = x;
-		oldy = y;
-		diff = abs(x-x1);
-		x++;
-		y += dy;
-		dy += d2y;
-		
-		//SetPixel(hdc, 100+x, 100+y, c);
-		BresenhamsEfficientDDA(hdc, 100+oldx, 100+oldy, 100+x, 100+y, c);
-		BresenhamsEfficientDDA(hdc, 100+oldx-2*diff, 100+oldy, 100+x-2*diff, 100+y, c);
-
-	}
-}
-
-
-void DraftbresenhamsDDALine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c) {
-	int dx = abs(x2 - x1);
-	int dy = abs(y2 - y1);
-	int x = x1;
-	int y = y1;
-
-	SetPixel(hdc, x, y, c);
-
-
-	while (x < x2) {
-
-		int d = (dx * (2*y+1 - 2*y1)) - (2 * dy * (x+1 - x1));
-		if (d > 0) {
-			x++;
-			SetPixel(hdc, x, y, c);
-		} else {
-			x++;
-			y++;
-			SetPixel(hdc, x, y, c);
-		}
-	}
 }
 
 void DrawLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c) {
